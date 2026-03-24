7d927f3f073262701c8fe70a2260677b4c32cc19
Vasanth.K <vasanth.k08@gmail.com>
2026-03-24 21:18:01 +0530

    coderun update

--- clients/web/apps/coderun-lite/src/App.jsx
+++ clients/web/apps/coderun-lite/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import CodeEditor from './components/CodeEditor'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div style={{textAlign: 'left', height: '100%', overflow: 'hidden'}}>
+    <div style={{textAlign: 'left', width: '100%', height: '100%', overflow: 'hidden'}}>
           <CodeEditor />
     </div>
   )
--- clients/web/apps/coderun-lite/src/components/CodeEditor.jsx
+++ clients/web/apps/coderun-lite/src/components/CodeEditor.jsx
@@ -1,25 +1,35 @@
-import  { useState } from 'react';
-import CodeMirror from '@uiw/react-codemirror';
+import  { useCallback, useState } from 'react';
+import CodeMirror , {keymap} from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { python } from '@codemirror/lang-python';
-import { php } from '@codemirror/lang-php';
+import { php } from "@codemirror/lang-php";
 import { PlayCircleFilled } from '@ant-design/icons';
-import { Tabs, Spin } from 'antd';
-import { renderToStaticMarkup } from 'react-dom/server';
+import { Tabs, Splitter } from 'antd';
 
-const ScriptPanel = ({lang})=>{
+const ScriptPanel = ({defaults, lang})=>{
 
-  const defaults = {
-    php : "echo 'Hello World..!';\n\n\n\n",
-    js : "console.log('Hello World...!');\n\n\n\n",
-    py : "print('Hello World...!')\n\n\n\n"
+  const [code, setCode] = useState(defaults[lang]['code']);
+  const [output, setOutput] = useState("...");
+  const [showShortcut, setShowShortcut] = useState(false);
+
+  const toggleShowShortcut = ()=>{
+    setShowShortcut(!showShortcut);
   }
-  const [code, setCode] = useState(defaults[lang]);
-  const [output, setOutput] = useState("Output: ");
 
   const onChange = (value, viewUpdate) => {
     setCode(value);
   };
+
+  const runWithShortcut = useCallback(async (v)=>{
+    await runCode();
+  });
+
+  const shortcutKeymap = keymap.of([
+    {
+      key: 'Alt-s',
+      run: runWithShortcut,
+    },
+  ]);
 
   const runCode = async ()=>{
 
@@ -33,43 +43,80 @@ const ScriptPanel = ({lang})=>{
                                   body: JSON.stringify({script: code.trim()})
                                 });
         const res = await(outp.text());
-        setOutput(`Output:<br><small> ${res.replace('\n','<br>')}<small>`);
+        setOutput(` ${res.replace('\n','<br>')}`);
   };
 
-  return <div style={{height: '100vh'}}>
-                <CodeMirror
-                    value={code}
-                    height='77vh '
-                    extensions={[
-                      (lang=='js') ? javascript() : ( (lang=='php') ? php() : python() )
-                    ]} 
-                    onChange={onChange}
-                />
-                <PlayCircleFilled onClick={runCode}  style={{position: 'absolute', bottom: '5rem', right: '3rem'}} />
-                <div style={{ width: '100%', height: '21vh', padding: '5px'}}  dangerouslySetInnerHTML={{ __html: output }} >
-                </div>
+  return <div style={{ height: '100%' }}>
+    
+                <Splitter vertical style={{ height: '83vh', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+                      <Splitter.Panel defaultSize="65%" min="35%" max="65%">
+                        <div style={{padding: '10px',}} >
+                            <CodeMirror
+                                value={code}
+                                autoFocus={true}
+                                extensions={[
+                                  shortcutKeymap, 
+                                  (lang=='js') ? javascript() : ( (lang=='php') ? php() : python() )
+                                ]} 
+                                onChange={onChange}
+                                onFocus={toggleShowShortcut}
+                                onBlur={toggleShowShortcut}
+                            />
+                        </div>
+                      </Splitter.Panel>
+                  <Splitter.Panel defaultSize="35%" min="35%" max="65%">
+                    <div style={{padding: '10px',}} >
+                        <div>
+                          <span style={{ backgroundColor: '#1677ff10', borderRadius: '1rem', padding: '3px 10px' }}> {defaults[lang]['name']} • Output Terminal • STDOUT</span>
+                          <PlayCircleFilled onClick={runCode}  style={{ color: '#1677ff', float: 'right', fontSize: '1.3rem', margin: '2px 10px' }} />
+                          <span style={{ float: 'right' }}>{showShortcut && <>[ <small style={{color: '#1677ff'}}>Alt+s</small> ] •</>} Execute</span>
+                          
+                      </div>
+                      <div style={{ width: '96.5%', backgroundColor: '#1677ff05', borderRadius: '5px', marginTop: '10px', padding: '10px', minHeight: '4.5rem'}}  dangerouslySetInnerHTML={{ __html: output }} >
+                      </div>
+                    </div>
+                    
+                  </Splitter.Panel>
+                </Splitter>
             </div>
+          
 }
 
 const CodeEditor = () => {
 
-  const editors = ['php', 'js', 'py'].map((lang)=>{ 
+  const defaults = {
+    php : {
+      name: 'PHP',
+      code: "echo 'Hello World..!';" + "\n".repeat(11)
+    },
+    js : {
+      name: 'JavaScript',
+      code: "console.log('Hello World...!');" + "\n".repeat(11)
+    },
+    py : {
+      name: 'Python',
+      code: "print('Hello World...!')" + "\n".repeat(11)
+    }
+  }
+
+  const editors = Object.keys(defaults).map((lang)=>{ 
     return {
-        label: lang.toUpperCase(),
+        label: defaults[lang]['name'],
         key: lang,
-        children: <ScriptPanel lang={lang} />
+        children: <ScriptPanel defaults={{[lang] : defaults[lang]}} lang={lang} />
     }
 
   });
 
   return (
-    <>
+    <div style={{ borderRadius: '5rem', padding: '10px' }}>
       <Tabs
-        style={{ width: '100%', height: '100vh' }}
-        tabPlacement='end'
+        size='small'
+        style={{ width: '100%' }}
+        tabPlacement='top'
         items={editors}
       />
-    </>
+    </div>
   );
 };
 
